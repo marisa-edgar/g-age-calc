aa5e92449662c8859394babcf5e8f54dbb522e7a
marisa-edgar <marisaedgar1212@gmail.com>
2021-11-12 09:20:19 -0800

    create environment

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './css/styles.css';
+import $ 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/styles.css';
+
+	plugins: [
+    		new CleanWebpackPlugin(), // new line
+    		new HtmlWebpackPlugin({
+      		  title: 'Shape Tracker',
