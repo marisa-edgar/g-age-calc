f2bf29fd46dad92f678d754e5a6011b04c5bc87d
marisa-edgar <marisaedgar1212@gmail.com>
2021-11-12 13:39:06 -0800

    pass second test

--- __test__/age.test.js
+++ __test__/age.test.js
@@ -14,7 +14,7 @@ describe('User',()=>{
     expect(user1.mercury).toEqual(133.33333333333334);
     expect(user1.venus).toEqual(51.612903225806456);
     expect(user1.mars).toEqual(17.02127659574468);
-    expect(user1.jupiter).toEqual(2.69);
+    expect(user1.jupiter).toEqual(2.69814502529511);
 
   })
 });
