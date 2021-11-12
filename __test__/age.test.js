f7a05f69b801d41b9fe0eb2fb32f4ac2da563f2e
marisa-edgar <marisaedgar1212@gmail.com>
2021-11-12 14:16:20 -0800

    third test fail

--- __test__/age.test.js
+++ __test__/age.test.js
@@ -15,6 +15,12 @@ describe('User',()=>{
     expect(user1.venus).toEqual(51.612903225806456);
     expect(user1.mars).toEqual(17.02127659574468);
     expect(user1.jupiter).toEqual(2.69814502529511);
-
+  })
+  test('should correctly return average life span',()=>{
+    
+    const user1 = new User("Risa",32);
+    expect(user1.name).toEqual("Risa");
+    expect(user1.age).toEqual(32);
+    expect(user1.expectancy).toEqual(72);
   })
 });
