66e09d050fa169cc883519a8214cbf818acccb23
marisa-edgar <marisaedgar1212@gmail.com>
2021-11-12 10:02:45 -0800

    fix environment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import './css/styles.css';
-import $ 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
-	plugins: [
-    		new CleanWebpackPlugin(), // new line
-    		new HtmlWebpackPlugin({
-      		  title: 'Shape Tracker',
+
